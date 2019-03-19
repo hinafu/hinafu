642f93beb4f1c3b2b1161ae2421ffd3a2ac5dcf5
Braulio Acosta Martínez <hinafu@gmail.com>
2019-03-19 13:51:48 -0500

    listen to her daddy's song

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,27 @@ $(document).ready(function() {
 	$(".header__background__list").slick({
 		autoplay: true,
 		arrows: false,
-		fade: true
+		fade: true,
+		speed: 500
 	});
 
-	var altoScroll = 200;
+	var altoScroll = window.height;
 	$(window).scroll(function() {
 		var iCurScrollPos = $(this).scrollTop();
 		if (iCurScrollPos > altoScroll) $(".section__menu").addClass("alt_menu");
 		else $(".section__menu").removeClass("alt_menu");
 
 		//altoScroll = iCurScrollPos;
+	});
+
+	$(".values").slider({
+		min: 0,
+		max: 100,
+		value: 50,
+		slide: function(e, ui) {
+			var opacity = ui.value;
+			$(".header__background__over").css("opacity", (opacity / 100));
+			$(".values_value").html(opacity + "%");
+		}
 	});
 });
